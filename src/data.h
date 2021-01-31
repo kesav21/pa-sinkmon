c82b34cfa3379834cf70053147fb05d1321240b1
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-31 16:16:14 -0700

    initial commit

---
+++ src/data.h
@@ -0,0 +1,8 @@
+#ifndef DATA_H
+#define DATA_H
+
+typedef struct {
+	int sink;
+} UserData;
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,7 @@
+#ifndef UITLS_H
+#define UITLS_H
+
+int pidof(char* name, int namelen);
+int intlen(int input);
+
+#endif
